b2abb7c0e0b9070c9f55ee8aa64e5aeb3bd642c9
yuri guemra <yuri.guemra@live.fr>
2023-12-11 17:00:37 +0100

    exercice BookController

--- BookStoreAPI/Controllers/BookController.cs
+++ BookStoreAPI/Controllers/BookController.cs
@@ -10,12 +10,19 @@ namespace BookStoreAPI.Controllers; // BookStoreAPI est l'espace de nom racine d
 // Ceci est une annotation, elle permet de définir des métadonnées sur une classe
 // Dans ce contexte elle permet de définir que la classe BookController est un contrôleur d'API
 // On parle aussi de decorator / décorateur
+
+//Cette classe BookController est le contrôleur d'API qui gère les requêtes relatives aux livres.
+
 [ApiController]
 public class BookController : ControllerBase
 {
+
+    //Cette annotation [HttpGet("books")] indique que cette méthode répond aux requêtes GET avec l'URL /books. Elle renvoie une ActionResult<List<Book>>, ce qui signifie que la méthode peut renvoyer différents types de résultats, tels que Ok(), NotFound(), etc. Dans ce cas, la méthode renvoie une liste de livres.
     [HttpGet("books")]
     public ActionResult<List<Book>> GetBooks()
     {
+
+        //Dans cette méthode, nous créons une liste de livres avec un seul livre. La liste est ensuite renvoyée en utilisant Ok(), qui renvoie un code de statut HTTP 200 OK avec la liste de livres comme corps de la réponse.
 
         var books = new List<Book>
         {
--- BookStoreAPI/obj/Debug/net8.0/BookStoreAPI.AssemblyInfo.cs
+++ BookStoreAPI/obj/Debug/net8.0/BookStoreAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4346d2f73e61a2ed59cd1853659ede7a78bb1bc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f2b947243eb62df1a6772d8cd48d7bc8c74d07c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
